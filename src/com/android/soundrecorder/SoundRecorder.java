fb35c52989e76c8dc350f9edc76bdc86e86de1fe
wjiang <wjiang@codeaurora.org>
2013-12-27 10:16:19 +0800

    SoundRecorder: Fix the recording file will be saved when powering off
    When powering off the DUT, the file has been saved.
    Delete the file at the case.
    
    Change-Id: I438d57cd677aa5591293d5466018b82b6baf76be
    CRs-Fixed: 564083

--- src/com/android/soundrecorder/SoundRecorder.java
+++ src/com/android/soundrecorder/SoundRecorder.java
@@ -1027,6 +1027,7 @@ public class SoundRecorder extends Activity
                     if (action.equals(Intent.ACTION_SHUTDOWN)) {
                         if (mRecorder != null) {
                             mRecorder.stop();
+                            mRecorder.delete();
                         }
                     }
                 }
