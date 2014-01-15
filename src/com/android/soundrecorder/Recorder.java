82a465343048954241e4cb3c94af4dc3507c7cec
wjiang <wjiang@codeaurora.org>
2014-01-15 13:26:59 +0800

    SoundRecorder: don't exit app once user accept recorded clip
    It's a requirement from Huiye to change the default behavior, in which
    SoundRecorder shouldn't exit after users click accept button.
    
    Change-Id: I352a989d487f728afbef22db630a286ad57c6b39
    CRs-Fixed: 593715

--- src/com/android/soundrecorder/Recorder.java
+++ src/com/android/soundrecorder/Recorder.java
@@ -153,6 +153,7 @@ public class Recorder implements OnCompletionListener, OnErrorListener {
     public void clear() {
         stop();
         
+        mSampleFile = null;
         mSampleLength = 0;
         
         signalStateChanged(IDLE_STATE);
