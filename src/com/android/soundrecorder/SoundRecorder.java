2f116ee16ff6a6693442cba9b3687d853bf4d947
zdi <zdi@codeaurora.org>
2016-02-22 14:11:53 +0800

    SoundRecorder: Add FLAG_GRANT_READ_URI_PERMISSION to result uri.
    Add FLAG_GRANT_READ_URI_PERMISSION to result uri, ensure it can be
    accessed in cross profile audio capture test.
    
    Change-Id: I4431be39b78af58e29a08c4fd130e053817a2aec
    CRs-Fixed: 956262

--- src/com/android/soundrecorder/SoundRecorder.java
+++ src/com/android/soundrecorder/SoundRecorder.java
@@ -1251,7 +1251,8 @@ public class SoundRecorder extends Activity
         // reset mRecorder and restore UI.
         mRecorder.clear();
         updateUi();
-        setResult(RESULT_OK, new Intent().setData(uri));
+        setResult(RESULT_OK, new Intent().setData(uri)
+                .setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION));
 
         showDialogAndExit(exit);
         return true;
