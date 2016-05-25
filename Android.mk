a17ad65dced515ac624e111d447fd30b85c42ec6
Chao Zhang <chaoz@codeaurora.org>
2016-05-25 12:29:44 +0800

    SoundRecorder: migration to android O
    1.UI enhancement
    2.add file list to display all the recording items
    3.support playback the recording files
    4.support more audio formats for CMCC
    5.upgrade SDK version and permissionfor security strategy
    6.add wrapper
    7.adjust layout for 8998 large screen size
    8.add internal interfaces for test team validation
    
    CRs-Fixed: 2020943
    
    Conflicts:
    	Android.mk
    	res/values-zh-rCN/codeaurora_strings.xml
    	res/values/codeaurora_dimens.xml
    	res/values/codeaurora_strings.xml
    	res/values/dimens.xml
    	src/com/android/soundrecorder/SoundRecorder.java
    	src/com/android/soundrecorder/util/DatabaseUtils.java
    	src/com/android/soundrecorder/util/FileUtils.java
    	src/com/android/soundrecorder/util/StorageUtils.java
    
    Change-Id: Ief666bdd94f8ebd18717a41c0637d3cbbe38d5a6

--- Android.mk
+++ Android.mk
@@ -1,10 +1,33 @@
 LOCAL_PATH:= $(call my-dir)
+
+# make wrapper static lib
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(call all-java-files-under, src-Wrapper)
+LOCAL_MODULE := soundrecorder_wrapper
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += frameworks/support/v7/recyclerview/res
+
+LOCAL_AAPT_FLAGS += --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages
+LOCAL_AAPT_FLAGS += android.support.v7.recyclerview
+
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
+LOCAL_STATIC_JAVA_LIBRARIES += soundrecorder_wrapper
+
 LOCAL_PACKAGE_NAME := SoundRecorder
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_PACKAGE)
